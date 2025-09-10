4f5557b78317714febcda07455aebd76079e8b5c
saarangp <saarangp@gmail.com>
2025-09-10 22:44:58 +0000

    Deploying to gh-pages from @ saarangp/saarangp.github.io@5d85d51d1838d7938a74f945682b2c9a61e81d5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -50,16 +50,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-started-an-internship-with-the-brainai-team-at-fair",
+          title: 'Started an internship with the BrainAI team at FAIR!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
