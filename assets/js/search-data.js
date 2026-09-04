6e27766b8514cd2345ea23cbaabe62291abfc40c
saarangp <saarangp@gmail.com>
2026-09-04 08:32:20 +0000

    Deploying to gh-pages from @ saarangp/saarangp.github.io@3346f5d389b222af12b47db68338bd8828a88cca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,13 +3,19 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-saarang-panchavati",
+    title: "Saarang Panchavati",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "news-started-an-internship-with-the-brainai-team-at-fair",
           title: 'Started an internship with the BrainAI team at FAIR!',
+          description: "",
+          section: "News",},{id: "news-defended-my-phd",
+          title: 'Defended my PhD!',
+          description: "",
+          section: "News",},{id: "news-started-as-a-research-scientist-at-google-deepmind-in-paris",
+          title: 'Started as a Research Scientist at Google DeepMind in Paris!',
           description: "",
           section: "News",},];
